e574dae6a6bdb35f0df533c5dff855919da2b425
Mohamed Yousuf <mohamed.a.youssif@gmail.com>
2024-08-21 17:18:36 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+// Navbar
+
+function toggleMenu() {
+  const toggleMenu = document.querySelector(".toggleMenu");
+  const navigation = document.querySelector(".navigation");
+  toggleMenu.classList.toggle("active");
+  navigation.classList.toggle("active");
+}
+
+// Slider
+var swiper = new Swiper(".testimonial-slider", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  autoplay:{
+    delay:2500,
+    disableOnInteraction:false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+ 
+    800: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
